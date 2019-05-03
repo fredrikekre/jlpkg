246510a84d86e12d501906e304e04b222e9aada7
Fredrik Ekre <ekrefredrik@gmail.com>
2019-05-03 12:57:32 +0200

    Simpler install.

--- src/jlpkg.jl
+++ src/jlpkg.jl
@@ -6,10 +6,10 @@ module jlpkg
 Install the command line interface.
 
 *Keyword arguments:*
- - `julia`: path to julia executable, defaults to the current
-   running julia.
- - `command`: name of the command, defaults to `jlpkg`.
- - `dir`: writable directory (available in PATH) for the executable,
+ - `command`: name of the executable command, defaults to `jlpkg`.
+ - `julia`: path to julia executable, defaults to the path of the
+   current running julia.
+ - `destdir`: writable directory (available in PATH) for the executable,
    defaults to `~/.julia/bin`.
  - `julia_flags`: vector with command line flags for the julia executable,
    defaults to `["--color=yes", "--startup-file=no", "-q"]`.
@@ -17,45 +17,43 @@ Install the command line interface.
 """
 function install(; julia::String=joinpath(Sys.BINDIR, Base.julia_exename()),
                    command::String="jlpkg",
-                   dir::String=joinpath(DEPOT_PATH[1], "bin"),
+                   destdir::String=joinpath(DEPOT_PATH[1], "bin"),
                    julia_flags::Vector{String}=["--color=yes", "--startup-file=no", "-q"],
                    force::Bool=false)
-    install_dir = normpath(joinpath(@__DIR__, "..", "jlpkg"))
-    jlpkg_scriptfile = joinpath(install_dir, "jlpkg.jl")
-    jlpkg_script = """
-        #!/usr/bin/env bash
-        exec $(julia) $(join(julia_flags, ' ')) '$(jlpkg_scriptfile)' "\$@"
-        """
-    h = hash(jlpkg_script)
-    exec_scriptfile = joinpath(install_dir, string("jlpkg", '-', h))
-    open(exec_scriptfile, "w") do f
-        print(f, jlpkg_script)
+    exec = joinpath(destdir, command)
+    if ispath(exec) && !force
+        error("file `$(exec)` already exists; use `jlpkg.install(force=true)` to overwrite.")
     end
-    chmod(exec_scriptfile, 0o0100775) # equivalent to -rwxrwxr-x (chmod +x exec_scriptfile)
-    link = joinpath(dir, command)
-    force && rm(link)
-    if ispath(link) || islink(link)
-        error("file `$link` already exists; use `jlpkg.install(force=true)` to overwrite.")
+    mkpath(destdir)
+    open(exec, "w") do f
+        print(f, """
+            #!/usr/bin/env bash
+            #=
+            exec $(julia) $(join(julia_flags, ' ')) "\${BASH_SOURCE[0]}" "\$@"
+            =#
+            """)
+        open(abspath(@__DIR__, "cli.jl"), "r") do cli
+            write(f, cli)
+        end
     end
-    mkpath(dir)
-    symlink(exec_scriptfile, link)
+    chmod(exec, 0o0100775) # equivalent to -rwxrwxr-x (chmod +x exec)
     if Sys.which(command) === nothing
         @warn """
               `Sys.which(\"$(command)\")` returns `nothing`, meaning that `command`
-              can not be found in PATH. Either make sure that `$(dir)` is in PATH,
+              can not be found in PATH. Either make sure that `$(destdir)` is in PATH,
               or manually add a symlink from a directory in PATH to the installed
               program file.
               Path to installed program:
-                  $(exec_scriptfile)
+                  $(exec)
               """
-    elseif realpath(Sys.which(command)) !== realpath(exec_scriptfile)
+    elseif realpath(Sys.which(command)) !== realpath(exec)
         @warn """
               `Sys.which(\"$(command)\")` points to a different program than the one
               just installed. Please check your PATH.
               `Sys.which(\"$(command)\")`:
                   $(Sys.which(command))
               Path to installed program:
-                  $(exec_scriptfile)
+                  $(exec)
               """
     end
     return
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,43 +1,46 @@
 using jlpkg, Test, Pkg, Pkg.TOML
 
-const flags = ["--color=yes", "--startup-file=no", "-q",
-    "--code-coverage=$(["none", "user", "all"][Base.JLOptions().code_coverage+1])"]
+const cli_script = joinpath(dirname(pathof(jlpkg)), "cli.jl")
+const test_cmd = ```$(Base.julia_cmd()) --color=yes --startup-file=no -q
+    --code-coverage=$(["none", "user", "all"][Base.JLOptions().code_coverage+1])
+    $(cli_script)```
 
 mktempdir(@__DIR__) do tmpdir
     withenv("PATH" => tmpdir * ':' * get(ENV, "PATH", "")) do
         # Installation
-        jlpkg.install(dir=tmpdir)
-        @test_throws ErrorException jlpkg.install(dir=tmpdir)
-        jlpkg.install(dir=tmpdir, julia_flags=flags; force = true)
-        jlpkg.install(command="pkg", dir=tmpdir, julia_flags=flags)
-        @test realpath(Sys.which("jlpkg")) ==
-              realpath(Sys.which("pkg")) ==
-              realpath(joinpath(tmpdir, "jlpkg"))
+        jlpkg.install(destdir=tmpdir)
+        @test_throws ErrorException jlpkg.install(destdir=tmpdir)
+        jlpkg.install(destdir=tmpdir, force = true)
+        jlpkg.install(command="pkg", destdir=tmpdir)
+        @test realpath(Sys.which("jlpkg")) == realpath(joinpath(tmpdir, "jlpkg"))
+        @test realpath(Sys.which("pkg")) ==  realpath(joinpath(tmpdir, "pkg"))
         @test_logs((:warn, r"can not be found in PATH"),
-            jlpkg.install(command="cmd-not-in-path", dir=joinpath(tmpdir, "dir-not-in-path")))
+            jlpkg.install(command="cmd-not-in-path", destdir=joinpath(tmpdir, "dir-not-in-path")))
         mktempdir(tmpdir) do tmpdir2; withenv("PATH" => get(ENV, "PATH", "") * ':' * tmpdir2) do
-            @test_logs (:warn, r"points to a different program") jlpkg.install(dir=tmpdir2)
+            @test_logs (:warn, r"points to a different program") jlpkg.install(destdir=tmpdir2)
         end end
         # Usage
-        @test success(`jlpkg --update --project=$tmpdir add Example=7876af07-990d-54b4-ab0e-23690620f79a`)
+        @test success(`jlpkg st`)
+        @test success(`pkg st`)
+        @test success(`$(test_cmd) --update --project=$tmpdir add Example=7876af07-990d-54b4-ab0e-23690620f79a`)
         project = TOML.parsefile(joinpath(tmpdir, "Project.toml"))
         @test project["deps"]["Example"] == "7876af07-990d-54b4-ab0e-23690620f79a"
         cd(tmpdir) do
-            @test success(`pkg --project add JSON=682c06a0-de6a-54ab-a142-c8b1cf79cde6`)
+            @test success(`$(test_cmd) --project add JSON=682c06a0-de6a-54ab-a142-c8b1cf79cde6`)
         end
         project = TOML.parsefile(joinpath(tmpdir, "Project.toml"))
         @test project["deps"]["JSON"] == "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
         # Output
         stdout, stderr = joinpath.(tmpdir, ("stdout.txt", "stderr.txt"))
-        @test success(pipeline(`jlpkg --help`, stdout=stdout, stderr=stderr))
-        @test occursin("jlpkg - Command line interface", read(stdout, String))
+        @test success(pipeline(`$(test_cmd) --help`, stdout=stdout, stderr=stderr))
+        @test occursin("jlpkg - A command line interface", read(stdout, String))
         @test isempty(read(stderr, String))
-        @test success(pipeline(`jlpkg`, stdout=stdout, stderr=stderr))
+        @test success(pipeline(`$(test_cmd)`, stdout=stdout, stderr=stderr))
         @test occursin("No input arguments, showing help", read(stdout, String))
-        @test occursin("jlpkg - Command line interface", read(stdout, String))
+        @test occursin("jlpkg - A command line interface", read(stdout, String))
         @test isempty(read(stderr, String))
         # Error paths
-        @test !success(pipeline(`jlpkg  --project=$tmpdir rm`, stdout=stdout, stderr=stderr))
+        @test !success(pipeline(`$(test_cmd)  --project=$tmpdir rm`, stdout=stdout, stderr=stderr))
         @test occursin("PkgError:", read(stdout, String))
         @test isempty(read(stderr, String))
     end
