1dea3de2092089aa614dda9ece3277a460830408
Fredrik Ekre <ekrefredrik@gmail.com>
2019-06-14 14:31:09 +0200

    Set version to v1.0.3.

--- src/cli.jl
+++ src/cli.jl
@@ -52,7 +52,7 @@ end
 
 # Parse --version option
 if "--version" in JLPKG_ARGS
-    println("jlpkg version 1.0.2, julia version $(VERSION)")
+    println("jlpkg version 1.0.3, julia version $(VERSION)")
     exit(0)
 end
 
